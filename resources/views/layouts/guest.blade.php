fdd3c6b0732eaf7bf80756edfc004c19fbfb6087
M. Homeister <micha@0x25.de>
2025-01-29 13:30:58 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,8 @@
     </head>
     <body class="font-sans text-gray-900 antialiased" style="background-color: #4158D0; background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-ultralight">
+            <x-icons.app width="300px"/>
+
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
