b9af6402cde8b5071c1cb40367cbdb47e072e67d
M. Homeister <micha@0x25.de>
2025-01-30 23:08:09 +0100

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" href="/favicon.png">
 
         <title>{{ ($metaTitle ? $metaTitle . ' | ' : '') . config('app.name', 'Laravel') }}</title>
 
